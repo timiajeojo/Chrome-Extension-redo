5a5f505a65b5447977da37baae36768bb8f36cda
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-12 18:37:07 -0500

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,24 @@
+body {
+    margin: 0;
+    padding: 10px;
+    font-family:  Arial, Helvetica, sans-serif;
+}
 
+input {
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid #5f9341;
+} 
+
+button {
+  background-color: #5f9341;
+  color: white;
+  padding: 10px 20px;
+  font-size: 14px;
+  font-weight: bold;
+  border: none;
+  border-radius: 8px;
+  margin-top: 4px;
+  
+}
