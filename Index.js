5cba930616d40c0a673823c7e6c28da05112165e
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-23 15:04:50 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,12 @@
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+  
+import { getDatabase,
+ref,
+push,
+onValue,
+remove } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
