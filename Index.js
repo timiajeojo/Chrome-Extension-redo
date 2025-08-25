2ef0e6c2d64a431d9a9b85a659c729fe07bb4fee
Oluwatimilehin AjeOjo <110703714+timiajeojo@users.noreply.github.com>
2025-08-24 21:07:21 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -10,3 +10,15 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxgD9GPxUb3UjWLPIiMIsmbdquvsnrX28",
+    authDomain: "leads-tracker-app-42d38.firebaseapp.com",
+    databaseURL: "https://leads-tracker-app-42d38-default-rtdb.firebaseio.com",
+    projectId: "leads-tracker-app-42d38",
+    storageBucket: "leads-tracker-app-42d38.firebasestorage.app",
+    messagingSenderId: "831225733252",
+    appId: "1:831225733252:web:6ad15e346540083a7ae2d1",
+    measurementId: "G-TDMTME86HH"
+  };
